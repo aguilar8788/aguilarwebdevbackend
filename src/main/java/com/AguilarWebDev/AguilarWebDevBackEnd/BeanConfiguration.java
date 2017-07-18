f4c294a70661c2f7ded1286c3afc61aa1898fabd
Peter <peter.aguilar2287@gmail.com>
2017-07-17 22:44:16 -0400

    make changes

--- src/main/java/com/AguilarWebDev/AguilarWebDevBackEnd/BeanConfiguration.java
+++ src/main/java/com/AguilarWebDev/AguilarWebDevBackEnd/BeanConfiguration.java
@@ -1,30 +1,30 @@
-//package com.AguilarWebDev.AguilarWebDevBackEnd;
-//
-//import com.mongodb.Mongo;
-//import com.mongodb.MongoClient;
-//import org.omg.CORBA.Environment;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.mongodb.MongoDbFactory;
-//import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
-//import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
-//import org.springframework.data.mongodb.gridfs.GridFsTemplate;
-//
-//@Configuration
-//public class BeanConfiguration extends AbstractMongoConfiguration {
-//
-//    @Bean
-//    public GridFsTemplate gridFsTemplate() throws Exception {
-//        return new GridFsTemplate(mongoDbFactory(), mappingMongoConverter());
-//    }
-//
-//    @Override
-//    protected String getDatabaseName() {
-//        return "test";
-//    }
-//
-//    @Override
-//    public Mongo mongo() throws Exception {
-//        return new MongoClient("127.0.0.1", 27017);
-//    }
-//}
+package com.AguilarWebDev.AguilarWebDevBackEnd;
+
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import org.omg.CORBA.Environment;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+import org.springframework.data.mongodb.gridfs.GridFsTemplate;
+
+@Configuration
+public class BeanConfiguration extends AbstractMongoConfiguration {
+
+    @Bean
+    public GridFsTemplate gridFsTemplate() throws Exception {
+        return new GridFsTemplate(mongoDbFactory(), mappingMongoConverter());
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return "test";
+    }
+
+    @Override
+    public Mongo mongo() throws Exception {
+        return new MongoClient("127.0.0.1", 27017);
+    }
+}
