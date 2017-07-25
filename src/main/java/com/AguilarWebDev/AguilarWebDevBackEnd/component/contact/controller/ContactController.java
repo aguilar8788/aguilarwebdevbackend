0544c2f5e583049aeb09531b1f23a176f9e4e788
Peter <peter.aguilar2287@gmail.com>
2017-07-24 22:45:06 -0400

    add changes

--- src/main/java/com/AguilarWebDev/AguilarWebDevBackEnd/component/contact/controller/ContactController.java
+++ src/main/java/com/AguilarWebDev/AguilarWebDevBackEnd/component/contact/controller/ContactController.java
@@ -3,6 +3,8 @@ package com.AguilarWebDev.AguilarWebDevBackEnd.component.contact.controller;
 import com.AguilarWebDev.AguilarWebDevBackEnd.component.SmtpMailSender;
 import com.AguilarWebDev.AguilarWebDevBackEnd.component.contact.model.Contact;
 import com.AguilarWebDev.AguilarWebDevBackEnd.component.contact.repository.ContactRepository;
+import com.sendgrid.SendGrid;
+import com.sendgrid.SendGridException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.MailSender;
@@ -16,8 +18,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/contact")
-//@CrossOrigin(origins = "http://localhost:3000")
-@CrossOrigin("https://aguilarwebdevelopment.com")
+@CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin("https://aguilarwebdevelopment.com")
 public class ContactController {
 
     @Autowired
@@ -34,15 +36,31 @@ public class ContactController {
 
     @RequestMapping(method= RequestMethod.POST)
     public ResponseEntity create(@RequestBody Contact contact) throws MessagingException {
-        String contactName = contact.getFirstName() + contact.getLastName();
+        String contactName = contact.getFirstName() + " " + contact.getLastName();
         String contactPhoneNumber = contact.getPhoneNumber();
         String contactCompany = "";
         if(contact.getCompany() != null){
-           contactCompany = contact.getCompany();
+            contactCompany = contact.getCompany();
         }
+        SendGrid sendgrid = new SendGrid("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");
+        SendGrid.Email welcomeMail = new SendGrid.Email();
+        welcomeMail.addTo("aguilarwebdevelopment@gmail.com");
+        welcomeMail.addToName("Peter Aguilar Jr.");
+        welcomeMail.setFrom("contactRequest@aguilarwebdevelopment.com");
+        welcomeMail.setSubject("New Contact Request");
+        welcomeMail.setText("A new request for client contact has been created. Please follow up with " + contactName + " " + contactPhoneNumber + " " + contactCompany);
+
+        try {
+            SendGrid.Response response = sendgrid.send(welcomeMail);
+            System.out.println(response.getMessage());
+        } catch (SendGridException sge) {
+            sge.printStackTrace();
+        }
+
+
 //        smtpMailSender.send("peter.aguilar2287@gmail.com", "New Client to Contact", contactName + " " + contactPhoneNumber + " " + contactCompany);
-         return ResponseEntity.ok(
-        contactRepo.save(contact));
+        return ResponseEntity.ok(
+                contactRepo.save(contact));
     }
 
     @RequestMapping(method= RequestMethod.DELETE, value="{id}")
