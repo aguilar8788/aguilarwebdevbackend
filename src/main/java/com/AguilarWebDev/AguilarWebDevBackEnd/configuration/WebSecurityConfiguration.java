c4e11577c062fa66a65c5bcf8eb2bf78b0689c34
Peter <peter.aguilar2287@gmail.com>
2017-08-01 11:54:12 -0400

    add security

---
+++ src/main/java/com/AguilarWebDev/AguilarWebDevBackEnd/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,25 @@
+package com.AguilarWebDev.AguilarWebDevBackEnd.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+       web.ignoring().antMatchers("/portfolio", "/blog", "/files/**", "/contact", "files/**");
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        System.out.println(System.getenv("auth"));
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password("re@dypl@y3r1").roles("USER");
+    }
+}
